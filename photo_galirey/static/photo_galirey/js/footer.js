7f41bbabf04a6df0a12f14a54cfc14222f455b07
HANM4 <gfy-gfy@mail.ru>
2023-04-11 12:21:36 +0500

    adding adaptability to templates

--- photo_galirey/static/photo_galirey/js/footer.js
+++ photo_galirey/static/photo_galirey/js/footer.js
@@ -1,4 +1,8 @@
+import {use_modal_window} from './modal_window.js';
+
 //маска для поля номер телефона в форме заказать звонок
 $("input[type='phone']").mask('+7 (999) 999-99-99');
+
+let use_modal_window_menu = use_modal_window('modal_window_menu', 'close_modal_window_menu', 'open_modal_window_menu');
 
 
